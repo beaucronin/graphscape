eaa77b168835eea2281d773103752d2dc112e904
Beau Cronin <beau.cronin@gmail.com>
2016-02-05 13:00:54 -0800

    socket

--- static/index.js
+++ static/index.js
@@ -121,12 +121,12 @@ function startPlayback() {
 
 window.onload = function() {
 	try {
-		socket = new WebSocket('ws://127.0.0.1:8080/');
-
+		// socket = new WebSocket('ws://127.0.0.1:8080/');
+		socket = new WebSocket('ws://graphscape.herokuapp.com:8080/');
 	} catch (err) {
 		console.log(err);
-		socket = new WebSocket('ws://graphscape.herokuapp.com:8080/');
 	}
+	console.log(socket);
 	socket.onmessage = function(event) {
 		processEvent(JSON.parse(event.data));
 		var data = JSON.parse(event.data);
